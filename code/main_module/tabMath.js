ff172a70f4be50a32bec3fa6e6ad2217c8824d63
nullTheCoder <stepan.gaidukevich@gmail.com>
2022-02-27 02:04:46 +0200

    multiply rune

--- code/main_module/tabMath.js
+++ code/main_module/tabMath.js
@@ -1,8 +1,8 @@
 mainMod["tab_math"] = {
     name: "tab_math",
-    colour: ['rgb(0, 150, 130)',
-     'rgb(0, 140, 110)',
-     'rgb(0, 130, 100)'],
+    colour: ['rgb(150, 150, 150)',
+     'rgb(110, 110, 110)',
+     'rgb(130, 130, 130)'],
 
     runes: {
         add: {
@@ -68,6 +68,79 @@ mainMod["tab_math"] = {
             ],
             activeFunction () {
                 return "num[0][num[1]] = a + b"
+            },
+        },
+        multiply: {
+            properties: [
+                {
+                    name: "toChange",
+                    type: "reference",
+                    accepted: ["number"],
+                },
+                {
+                    name: "a",
+                    type: "referencable",
+                    accepted: ["number"],
+                    default: 0
+                },
+                {
+                    name: "b",
+                    type: "referencable",
+                    accepted: ["number"],
+                    default: 0
+                }
+            ],
+            draw(x, y, size, ctx) {
+                ctx.beginPath()
+
+                ctx.moveTo(x + (size*0.4), y + (size*0.4));
+                ctx.lineTo(x - (size*0.4), y - (size*0.4));
+
+                ctx.moveTo(x + (size*0.4), y - (size*0.4));
+                ctx.lineTo(x - (size*0.4), y + (size*0.4));
+
+                ctx.moveTo(x, y + (size*0.6));
+                ctx.lineTo(x, y - (size*0.6));
+
+                ctx.moveTo(x + (size*0.6), y);
+                ctx.lineTo(x - (size*0.6), y);
+
+
+                
+                ctx.stroke()
+                ctx.fill()
+            },
+            sendToFunction: [
+                {
+                    type: "property",
+                    property: "a",
+                    name: "a",
+                    default: 0,
+                    convert (str) {
+                        return str
+                    }
+                },
+                {
+                    type: "property",
+                    property: "b",
+                    name: "b",
+                    default: 0,
+                    convert (str) {
+                        return str
+                    }
+                },
+                {
+                    type: "reference",
+                    property: "toChange",
+                    name: "num",
+                    default: 0,
+                    convert (str) {
+                        return str
+                    }
+                }
+            ],
+            activeFunction () {
+                return "num[0][num[1]] = a * b"
             },
         }
     }
